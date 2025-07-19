b3b2aa048ec2f23ba7ad0921b2e07349342dc9eb
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-19 23:35:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,10 +18,34 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# Create a security group that allows SSH
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing all access for assingment only.
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Create EC2 instance with latest Ubuntu AMI
 resource "aws_instance" "ubuntu_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = "msd_assignment_key" # Ensure this key is already created in your AWS account
+
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   tags = {
     Name = "UbuntuWebServer1"
