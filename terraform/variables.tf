ea81dba149836b412c2cb4486201caf988ea8875
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-20 16:36:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
     default = "ap-south-1" #Mumbai region. Local to me
     type = string
 }
+
+variable "db_password" {
+  description = "Password for the RDS PostgreSQL database"
+  type        = string
+  sensitive   = true
+}
