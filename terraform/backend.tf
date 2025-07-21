5ff5ff30d84f67504ab2cada09f02bb067eb2106
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-21 10:39:36 +0530

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "msd-assignment-bucket"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "msd-assignment-table"
+  }
+}
