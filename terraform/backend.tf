32e9a2dbbe2ddadcf451c99d00109274024716c4
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-22 12:31:55 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "msd-assignment-bucket"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true             # Enable encryption at rest
-    dynamodb_table = "msd-assignment-table"
+    bucket         = ""
+    key            = ""
+    region         = ""
+    encrypt        = ""
+    dynamodb_table = ""
   }
 }
