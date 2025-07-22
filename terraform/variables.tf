d0d99910c28d6ed6828836b58a1cf84244665d31
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-22 12:55:26 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,13 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "application_load_balancer_name" {
+    description = "Application Load Balancer"
+    type = string
+}
+
+variable "application_load_balancer_region" {
+    description = "Application Load Balancer Region"
+    type = string
+}
