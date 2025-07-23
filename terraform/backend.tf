5f379bc7b59e55c1ac2391654692853709a17790
gokulan-g <158612270+gokulan-g@users.noreply.github.com>
2025-07-23 10:01:23 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
+  # Configure remote backend to store Terraform state in S3
   backend "s3" {
-    bucket         = ""
-    key            = ""
-    region         = ""
-    encrypt        = ""
-    dynamodb_table = ""
+    bucket         = ""  # Name of the S3 bucket to store the state file
+    key            = ""  # Path to the state file within the bucket 
+    region         = ""  # AWS region where the S3 bucket is located 
+    encrypt        = ""  # Whether to encrypt the state file at rest
+    dynamodb_table = ""  # DynamoDB table for state locking and consistency
   }
 }
